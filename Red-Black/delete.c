d27d5bf40acb507b08075caebf4556e3d644f894
Arthur Paul Nickel <apn25@inf.ufpr.br>
2026-06-20 12:27:36 -0300

    [add] Delete Fix Up

--- Red-Black/delete.c
+++ Red-Black/delete.c
@@ -86,7 +86,65 @@ void delete_RB (TreeRB *T, NoRB *z)
         delete_fixup_RB(T, x);
 }
 
-void delete_fixup_RB ()
+void delete_fixup_RB (TreeRB *T, NoRB *x)
 {
-    
+    NoRB *w;
+    //Enquanto x não for raiz e x for preto
+    while ( x != T->root && x->color == BLACK )
+    {
+        if ( x == x->father->left )
+        {
+            w = x->father->right;
+
+            //Se o irmão for vermelho
+            if ( w->color == RED )
+            {
+                rot_left(T, x->father); //rotaciona pai
+                //pinta irmão de preto e pai de vermelho
+                    //Manter alturas pretas
+                w->color = BLACK; //O pai era com certeza preto -> então pinta para manter altura preta da subarvore à direita
+                x->father->color = RED; //manter altura preta intacta no caminho do novo irmão de x
+
+                //ERRO: Colocar w = x->father->right aqui (vai cair em algum caso em baixo)
+            }
+            //irmão preto
+            //Se o irmão tem filhos pretos
+            //ERRO: Aqui não é else if -> o caso 1 sempre será seguido de outro
+            else if ( w->left->color == BLACK && w->right->color == BLACK)
+            {
+                w->color = RED; //pinta irmão de vermelho
+                x = x->father;  //x recebe pai
+            }
+            else
+            {
+                //Se o irmão tem filho desalinhado vermelho e filho alinhado preto
+                //ERRO: Não precisa da primeira verificação -> black black já foi conferido em cima
+                if( w->left->color == RED && w->right->color == BLACK )
+                {
+                    w = rot_right(T, w);    //rotação simples no irmão
+                    //irmão recebe veremlho e sobrinho preto (inverteu o parentesco)
+                    w->color = BLACK;
+                    w->right->color = RED;
+                }
+                //Se o irmão tem filho alinhado vermelho
+                rot_left(x->father);    //rotação no pai
+
+                //irmão (novo avô) recebe cor do pai -> não mudar para cima
+                w->color = x->father->color;
+
+                //pai recebe preto -> fim do duplo preto
+                x->father->color = BLACK;
+
+                //sobrinho recebe preto -> não mudar altura preta na árvore do irmão
+                w->right->color = BLACK;
+
+                x = T->root;    //para terminar o laço
+            }
+        }
+        else
+        {
+            //simétrico
+        }
+    }
+    x->color = BLACK;   //pinta x de vermelho -> pinta nó veremlho (pode ser raiz!)
 }
